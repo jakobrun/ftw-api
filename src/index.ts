39f5ec926b384988624e6a298ed932c49d94c0ee
jakob <jakob.runarsson@gmail.com>
2017-07-13 11:52:39 +0000

    set dev dependencies on dependency list so it can build on heroku

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,4 @@ import * as pgPromise from 'pg-promise'
 
 const db = pgPromise()(process.env.DATABASE_URL || process.env.FTW_DATABASE_URL || '')
 
-createApp(db).listen(3000)
+createApp(db).listen(process.env.PORT || 3000)
