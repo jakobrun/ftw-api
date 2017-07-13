5189f8818dd1cb8a4448b2d915505ec226ffe31a
jakob <jakob.runarsson@gmail.com>
2017-07-13 11:35:12 +0000

    prepare for heroku deployment

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,6 @@
 import { createApp } from './app'
 import * as pgPromise from 'pg-promise'
 
-const connecionOptions = {
-    database: 'ftw'
-}
-const db = pgPromise()(connecionOptions)
+const db = pgPromise()(process.env.DATABASE_URL || process.env.FTW_DATABASE_URL || '')
 
 createApp(db).listen(3000)
